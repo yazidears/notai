2228fa702634a489adffd0e8131e45f8891f7e32
yzde <uwuzide@gmail.com>
2023-02-08 16:37:14 +0100

    added launch

---
+++ main.py
@@ -0,0 +1,21 @@
+print("notAI")
+print("made by yazidears, maaarinaaa and ")
+print("check it out on https://github.com/yazidears/notai")
+print("Importing data, please wait.")
+import os
+import time
+import functions
+import random
+import sys
+import json
+import requests
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+print("Imported data.")
+print("Welcome to notAI!")
+print("Who are you, what's your name?")
+name = input("->")
+print("Welcome, " + name )
+time.sleep(2)
+clear()
+print("")
